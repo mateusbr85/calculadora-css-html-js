4364626dfc2c08f5d85da1ee22c15c9aee8be357
Mateus Veloso <67664486+mateusbr85@users.noreply.github.com>
2022-06-22 00:59:00 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,6 @@
+const numberButtons = document.querySelectorAll(['data-number']);
+const operationsButtons = document.querySelectorAll(['data-operator']);
+const equalsButtons = document.querySelector(['data-equals']);
+const deleteButtons = document.querySelector(['data-delete']);
+const previousOperand = document.querySelector(['data-previous-operand']);
+const currentOperand = document.querySelector(['data-current-operand']);
